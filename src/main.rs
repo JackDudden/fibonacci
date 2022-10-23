2ea9b56fa991c6ec160a93995ff2ad96adb98ace
Jack Dudden <jack@dudden.uk>
2022-10-23 15:38:09 +0100

    feat: Fibonacci calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let n: i32 = match &args[1].trim().parse() {
+        Ok(v) => *v,
+        Err(_) => {
+            println!("First argument must be an integer");
+            return;
+        }
+    };
+
+    let result = fibonnaci(n);
+
+    println!("Fibonnaci number for {n} is {result}");
+}
+
+fn fibonnaci(n: i32) -> i32 {
+    if [0, 1].contains(&n) {
+        n
+    } else {
+        fibonnaci(n - 1) + fibonnaci(n - 2)
+    }
 }
